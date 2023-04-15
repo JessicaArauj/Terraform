8f87ca42aa1bdcb58dd9de320a7d0f56107eb595
Jessica Teixeira Araujo <111321791+JessicaTeixeiraAraujo@users.noreply.github.com>
2023-04-15 15:31:36 -0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform{
+    required_providers {
+      aws = {
+        source = 'hashicorp/aws'
+        version = "~> 4.16"
+      }
+    }
+
+    redrequired_version = ">= 1.2.0"
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+resourresource "aws_s3_bucket" "s3_bucket" {
+    bucket = "tcb-app-qa-jr10"
+}
+
